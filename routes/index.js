6484aeb5bf8aab9585ab1258909e553f6232c54f
Krzysztof Dziedzic <34074568+krzychudz@users.noreply.github.com>
2020-12-05 16:51:51 +0100

    Improve comments

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,13 @@ router.get('/', (req, res) => {
 
 router.get('/api/blog-post', BlogPostController.getPosts); // Get all posts
 router.get('/api/:user_id/blog-post', BlogPostController.getUserPosts); // Get all user posts
-router.get('/api/:user_id/blog-post/post_id', BlogPostController.getUserPost) // Get paticualr post
+router.get('/api/:user_id/blog-post/post_id', BlogPostController.getUserPost) // Get particular post
 
 router.post('/api/:user_id/blog-post', BlogPostController.createBlogPost); // Create a new post for a user
 
 router.patch('/api/:user_id/blog-post/:post_id', BlogPostController.updatePost); // Update an existing post for paticular user
 
-router.delete('/api/:user_id/blog-post/:post_id', BlogPostController.updatePost); // Remove paticular post
+router.delete('/api/:user_id/blog-post/:post_id', BlogPostController.updatePost); // Remove particular post
 
 
 
