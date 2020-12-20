3aa842403eaf9156ef449438fc8302a9af58ab5e
Krzysztof Dziedzic <34074568+krzychudz@users.noreply.github.com>
2020-12-20 23:14:06 +0100

    Implement getUserProduct endpoint

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -1,4 +1,5 @@
 const { json } = require('body-parser');
+const { join } = require('path');
 const { traceDeprecation } = require('process');
 const firestoreDb = require('../firebase');
 const idGenerator = require('../helpers/IdGenerator');
@@ -7,10 +8,12 @@ const idGenerator = require('../helpers/IdGenerator');
 exports.createProduct = async (req, res) => {
     const userId = req.params.user_id;
     const body = req.body;
+    const productId = idGenerator.generateUniqueFirestoreId();
 
     body.ownerId = userId;
+    body.productId = productId;
 
-    await firestoreDb.collection("Products").add(body);
+    await firestoreDb.collection("Products").doc(productId).set(body);
     res.status(201).json(body);
   
 };
@@ -44,7 +47,15 @@ exports.getUserProducts = async (req, res) => {
 
 exports.getUserProduct = (req, res) => {
     const userId = req.params.user_id;
-    const productiId = req.params.product_id;
+    const productId = req.params.product_id;
+
+    const querySnapshot = await firestoreDb.collection("Products").where("ownerId", "==", userId).where("productId", "==", productId).get();
+
+    const productsResponse = querySnapshot.docs.map((document) => {
+        return {...document.data()}
+    });
+
+    res.status(201).json(productsResponse);
 }
 
 exports.updateProduct = (req, res) => {
