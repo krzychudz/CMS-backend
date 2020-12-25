f05ab7750380168ee3e668a59075cb626a562991
Krzysztof Dziedzic <34074568+krzychudz@users.noreply.github.com>
2020-12-25 18:06:53 +0100

    Update routes names

--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,14 @@ router.get('/', (req, res) => {
 
 
 router.get('/api/products', ProductsController.getProducts); // Get all products
-router.get('/api/:user_id/products', ProductsController.getUserProducts); // Get all user products
-router.get('/api/:user_id/products/:product_id', ProductsController.getUserProduct) // Get particular product
+router.get('/api/users/:user_id/products', ProductsController.getUserProducts); // Get all user products
+router.get('/api/users/:user_id/products/:product_id', ProductsController.getUserProduct) // Get particular product
 router.get('/api/find_product?query', ProductsController.findProduct) // Get products that match a given query
 
-router.post('/api/:user_id/products', ProductsController.createProduct); // Create a new product for a user
+router.post('/api/users/:user_id/products', ProductsController.createProduct); // Create a new product for a user
 
-router.patch('/api/:user_id/products/:product_id', ProductsController.updateProduct); // Update an existing product for paticular user
+router.patch('/api/users/:user_id/products/:product_id', ProductsController.updateProduct); // Update an existing product for paticular user
 
-router.delete('/api/:user_id/products/:product_id', ProductsController.deleteProduct); // Remove particular product
+router.delete('/api/users/:user_id/products/:product_id', ProductsController.deleteProduct); // Remove particular product
 
 module.exports = router;
