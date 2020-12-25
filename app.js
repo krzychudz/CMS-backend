f79db633063d89825318433db164af9869e6c3e6
Krzysztof Dziedzic <34074568+krzychudz@users.noreply.github.com>
2020-12-25 18:26:42 +0100

    Allow cors response

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes/index.js');
+var cors = require('cors')
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(cors())
 
 app.use('/', routes);
 
