5041a083053dbf8ba5e76903d4d306b0a40a087c
Krzysztof Dziedzic <34074568+krzychudz@users.noreply.github.com>
2021-01-16 18:56:57 +0100

    Replace characters in credentials

--- firebase.js
+++ firebase.js
@@ -1,8 +1,8 @@
 const firebase = require('firebase-admin');
-const serviceAccount = require('./firebase_key.json');
+//const serviceAccount = require('./firebase_key.json');
 
 firebase.initializeApp({
- credential: firebase.credential.cert(process.env.GOOGLE_APPLICATION_CREDENTIALS)
+ credential: firebase.credential.cert(process.env.GOOGLE_APPLICATION_CREDENTIALS.replace(/\\n/g, '\n'))
 });
 
 exports.firebaseDb = firebase.firestore();
